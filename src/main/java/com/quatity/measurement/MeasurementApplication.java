9265600cdd02fa928b3fb8f1de5599c07519e05e
Seetu2424 <seetubaghel16@gmail.com>
2026-05-06 10:36:59 +0530

    uc8

--- src/main/java/com/quatity/measurement/MeasurementApplication.java
+++ src/main/java/com/quatity/measurement/MeasurementApplication.java
@@ -9,11 +9,10 @@ import com.quatity.measurement.models.QuantityLength;
 public class MeasurementApplication {
 	
 	public static void main(String[] args) {
-	
+
 		QuantityLength q1 = new QuantityLength(2.0, LengthUnit.CENTIMETERS);
 		QuantityLength q2 = new QuantityLength(2.0, LengthUnit.YARDS);
 
 		System.out.println(q1.equals(q2));
-		
 		}
 }
