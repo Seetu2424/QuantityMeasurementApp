156b822f4176d5275463095b585951e9fb4b3864
Seetu2424 <seetubaghel16@gmail.com>
2026-05-06 11:31:05 +0530

    uc13

--- src/main/java/com/quatity/measurement/models/QuantityWeight.java
+++ src/main/java/com/quatity/measurement/models/QuantityWeight.java
@@ -32,11 +32,12 @@ public class QuantityWeight {
         Quantity<WeightUnit> result = quantity.toConvert(targetUnit);
         return new QuantityWeight(result.getValue(), result.getUnit());
     }
-@Override
-public boolean equals(Object o) {
-    if (!(o instanceof QuantityWeight)) {return false;
-    }
-    return this.quantity.equals(((QuantityWeight) o).quantity);
-}
 
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof QuantityWeight)) {
+            return false;
+        }
+        return this.quantity.equals(((QuantityWeight) o).quantity);
+    }
 }
