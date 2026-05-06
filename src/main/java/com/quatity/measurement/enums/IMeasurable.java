3c214640eef912edc12e2e7bb0b15418dcd76eb7
Seetu2424 <seetubaghel16@gmail.com>
2026-05-06 10:57:50 +0530

    uc10

---
+++ src/main/java/com/quatity/measurement/enums/IMeasurable.java
@@ -0,0 +1,11 @@
+package com.quatity.measurement.enums;
+
+public interface IMeasurable {
+
+	 double getConversionFactor();
+
+	    double convertToBaseUnit(double value);
+
+	    double convertFromBaseUnit(double value);
+	
+}
--- src/main/java/com/quatity/measurement/enums/LengthUnit.java
+++ src/main/java/com/quatity/measurement/enums/LengthUnit.java
@@ -1,31 +1,38 @@
 package com.quatity.measurement.enums;
 
-public enum LengthUnit {
-	
+public enum LengthUnit implements IMeasurable {
 
-	 FEET(1.0),
-	    INCH(1.0 / 12),
-	    YARDS(3.0),
-	    CENTIMETERS(1.0 / 30.48);
+    FEET(1.0),
+    INCH(1.0 / 12),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
 
-	    private final double toFeetFactor;
+    private final double toFeetFactor;
 
-	    LengthUnit(double toFeetFactor) {
-	        this.toFeetFactor = toFeetFactor;
-	    }
+    LengthUnit(double toFeetFactor) {
+        this.toFeetFactor = toFeetFactor;
+    }
 
-	    public double convertToBaseUnit(double value) {
-	        if (!Double.isFinite(value)) {
-	            throw new IllegalArgumentException("Invalid value");
-	        }
-	        return value * toFeetFactor;
-	    }
+    @Override
+    public double getConversionFactor() {
+        return toFeetFactor;
+    }
 
-	    public double convertFromBaseUnit(double value) {
-	        if (!Double.isFinite(value)) {
-	            throw new IllegalArgumentException("Invalid value");
-	        }
-	        return value / toFeetFactor;
-	    }
-    
+    @Override
+    public double convertToBaseUnit(double value) {
+        validate(value);
+        return value * toFeetFactor;
+    }
+
+    @Override
+    public double convertFromBaseUnit(double value) {
+        validate(value);
+        return value / toFeetFactor;
+    }
+
+    private void validate(double value) {
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid value");
+        }
+    }
 }
